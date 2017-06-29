e059b970b8232f649213d626c98a02978002b3d3
patilr <ravi86.patil@gmail.com>
2017-06-29 12:24:49 -0400

    Initial COMMIT

---
+++ src/test/java/Test1.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by patilr on 6/28/2017.
+ */
+public class Test1 {
+    @Test
+    public void test(){
+       System.out.println("This is first Test");
+    }
+}
